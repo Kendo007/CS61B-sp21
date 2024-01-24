ae257841ac59b72d45886d17e5cbbbe1fd3c53fd
Kendo007 <110466301+Kendo007@users.noreply.github.com>
2024-01-24 21:45:05 +0530

    lab2 done

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -1,7 +1,10 @@
 package IntList;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class SquarePrimesTest {
 
@@ -11,10 +14,34 @@ public class SquarePrimesTest {
      * is broken. Write your own JUnit Test to try to uncover the bug!
      */
     @Test
-    public void testSquarePrimesSimple() {
+    public void testSquarePrimesSingle() {
         IntList lst = IntList.of(14, 15, 16, 17, 18);
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesMultiple() {
+        IntList lst = IntList.of(14, 15, 16, 17, 23);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 16 -> 289 -> 529", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesFirstLast() {
+        IntList lst = IntList.of(3, 15, 16, 17, 23);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("9 -> 15 -> 16 -> 289 -> 529", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testNoPrimes() {
+        IntList lst = IntList.of(4, 15, 16, 18, 20);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 15 -> 16 -> 18 -> 20", lst.toString());
+        assertFalse(changed);
     }
 }
