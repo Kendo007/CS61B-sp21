daba0dc611d10db8876821f6e417a31436e5c7ff
Kendo007 <110466301+Kendo007@users.noreply.github.com>
2024-02-02 11:46:48 +0530

    Implemented Buggy nextnumber in Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,12 +3,14 @@
  */
 public class Collatz {
 
-    /** Returns a number of Collatz sequence based on the given number eg 5 -> 16, 12 -> 6 */
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n % 2 != 0) {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
             return 3 * n + 1;
         } else {
-            return n / 2;
+            return n * 2;
         }
     }
 
